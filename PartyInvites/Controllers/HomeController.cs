2140b5c6e3ed59d9e247308e487332f06967cb4b
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-08-21 22:42:50 +0300

    Change method RsvpForm() [HttpPost]

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -87,6 +87,31 @@ namespace PartyInvites.Controllers
 
         #region Append method RsvpForm() [HttpPost]
 
+        //public ViewResult Index()
+        //{
+        //    int hour = DateTime.Now.Hour;
+        //    ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+
+        //    return View("MyView");
+        //}
+
+        //[HttpGet]
+        //public ViewResult RsvpForm()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public ViewResult RsvpForm(GuestResponse guestResponse)
+        //{
+        //    // Что сделать: сохранить ответ от гостя
+        //    return View();
+        //}
+
+        #endregion
+
+        #region Change method RsvpForm() [HttpPost]
+
         public ViewResult Index()
         {
             int hour = DateTime.Now.Hour;
@@ -104,11 +129,13 @@ namespace PartyInvites.Controllers
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            // Что сделать: сохранить ответ от гостя
-            return View();
+            Repository.AddResponse(guestResponse);
+
+            return View("Thanks", guestResponse);
         }
 
         #endregion
+
 
 
     }
