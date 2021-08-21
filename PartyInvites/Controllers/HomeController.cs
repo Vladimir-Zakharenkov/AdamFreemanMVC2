f3bf311de44ac4f7a9a8f70955d6ec476babada4
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-08-22 00:10:25 +0300

    Append ModelState.IsValid

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -138,6 +138,37 @@ namespace PartyInvites.Controllers
 
         #region Append method ListResponses()
 
+        //public ViewResult Index()
+        //{
+        //    int hour = DateTime.Now.Hour;
+        //    ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+
+        //    return View("MyView");
+        //}
+
+        //[HttpGet]
+        //public ViewResult RsvpForm()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public ViewResult RsvpForm(GuestResponse guestResponse)
+        //{
+        //    Repository.AddResponse(guestResponse);
+
+        //    return View("Thanks", guestResponse);
+        //}
+
+        //public ViewResult ListResponses()
+        //{
+        //    return View(Repository.Responses.Where(r => r.WillAttend == true));
+        //}
+
+        #endregion
+
+        #region Append ModelState.IsValid
+
         public ViewResult Index()
         {
             int hour = DateTime.Now.Hour;
@@ -155,9 +186,19 @@ namespace PartyInvites.Controllers
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            Repository.AddResponse(guestResponse);
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // Обнаружена ошибка проверки достоверности.
+                return View();
+            }
 
-            return View("Thanks", guestResponse);
+
+
         }
 
         public ViewResult ListResponses()
@@ -166,6 +207,9 @@ namespace PartyInvites.Controllers
         }
 
         #endregion
+
+
+
 
 
 
--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -1,10 +1,42 @@
-﻿namespace PartyInvites.Models
+﻿#region Append Model
+
+//namespace PartyInvites.Models
+//{
+//    public class GuestResponse
+//    {
+//        public string Name { get; set; }
+//        public string Email { get; set; }
+//        public string Phone { get; set; }
+//        public bool? WillAttend { get; set; }
+//    }
+//}
+
+#endregion
+
+#region Change in Model
+
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
 {
     public class GuestResponse
     {
-        public string Name {  get; set; }
-        public string Email {  get; set; }
-        public string Phone {  get; set; }
-        public bool? WillAttend {  get; set; }
+        [Required(ErrorMessage ="Pleas enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage ="Please enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Please enter your phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage ="Please specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
     }
 }
+
+#endregion
+
+
+
