eb1968b6920e81717465b6338c8eb44aa2f92e9a
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-08-21 23:19:30 +0300

    Append View "ListResponses"

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -112,6 +112,32 @@ namespace PartyInvites.Controllers
 
         #region Change method RsvpForm() [HttpPost]
 
+        //public ViewResult Index()
+        //{
+        //    int hour = DateTime.Now.Hour;
+        //    ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+
+        //    return View("MyView");
+        //}
+
+        //[HttpGet]
+        //public ViewResult RsvpForm()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public ViewResult RsvpForm(GuestResponse guestResponse)
+        //{
+        //    Repository.AddResponse(guestResponse);
+
+        //    return View("Thanks", guestResponse);
+        //}
+
+        #endregion
+
+        #region Append method ListResponses()
+
         public ViewResult Index()
         {
             int hour = DateTime.Now.Hour;
@@ -134,7 +160,13 @@ namespace PartyInvites.Controllers
             return View("Thanks", guestResponse);
         }
 
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+
         #endregion
+
 
 
 
