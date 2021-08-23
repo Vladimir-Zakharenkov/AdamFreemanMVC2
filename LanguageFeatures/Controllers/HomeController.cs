98c11ed4efd265aa5d58db9455cb9883a5889963
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-08-23 22:19:20 +0300

    Null conditional operator

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -3,14 +3,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LanguageFeatures.Models;
 
 namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
+        #region Append first controller
+
+
+        //    public ViewResult Index()
+        //    {
+        //        return View(new string[] { "C#", "Language", "Features" });
+        //    }
+        //}
+
+        #endregion
+
+        #region Null conditional operator
+
         public ViewResult Index()
         {
-            return View(new string[] { "C#", "Language", "Features" });
+            List<string> results = new List<string>();
+
+            foreach (Product p in Product.GetProduct())
+            {
+                string name = p?.Name;
+                decimal? price = p?.Price;
+                results.Add(string.Format("Name: {0}, Price: {1}", name, price));
+            }
+
+            return View(results);
         }
+
+        #endregion
+
     }
+
+
+
+
+
 }
