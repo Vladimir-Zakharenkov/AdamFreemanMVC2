a882561c928d7037eab1379d69dcbd5b242d035b
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-08-23 22:51:26 +0300

    Adding a Property in the Product.cs File

--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -7,8 +7,35 @@ namespace LanguageFeatures.Models
 {
     public class Product
     {
+        #region Initial model
+
+        //public string Name { get; set; }
+        //public decimal? Price { get; set; }
+
+        //public static Product[] GetProduct()
+        //{
+        //    Product kayak = new Product
+        //    {
+        //        Name = "Kayak",
+        //        Price = 275M
+        //    };
+
+        //    Product lifejacket = new Product
+        //    {
+        //        Name = "Lifejacket",
+        //        Price = 48.95M
+        //    };
+
+        //    return new Product[] { kayak, lifejacket, null };
+        //}
+
+        #endregion
+
+        #region Adding a Property in the Product.cs File
+
         public string Name { get; set; }
         public decimal? Price { get; set; }
+        public Product Related {  get; set; }
 
         public static Product[] GetProduct()
         {
@@ -24,7 +51,12 @@ namespace LanguageFeatures.Models
                 Price = 48.95M
             };
 
+            kayak.Related = lifejacket;
+
             return new Product[] { kayak, lifejacket, null };
         }
+
+        #endregion
+
     }
 }
