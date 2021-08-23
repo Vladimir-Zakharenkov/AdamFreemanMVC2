58855e07202da5608a251841b5c812b7232b51f4
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-08-23 23:14:38 +0300

    Combining Null Operators

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -40,18 +40,38 @@ namespace LanguageFeatures.Controllers
 
         #region Detecting Nested null Values in the HomeController.cs File
 
+        //public ViewResult Index()
+        //{
+        //    List<string> results = new List<string>();
+
+        //    foreach (Product p in Product.GetProducts())
+        //    {
+        //        string name = p?.Name;
+        //        decimal? price = p?.Price;
+        //        string relatedName = p?.Related?.Name;
+        //        results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}",
+        //        name, price, relatedName));
+        //    }
+        //    return View(results);
+        //}
+
+        #endregion
+
+        #region Combining Null Operators
+
         public ViewResult Index()
         {
             List<string> results = new List<string>();
 
             foreach (Product p in Product.GetProducts())
             {
-                string name = p?.Name;
-                decimal? price = p?.Price;
-                string relatedName = p?.Related?.Name;
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
                 results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}",
                 name, price, relatedName));
             }
+
             return View(results);
         }
 
