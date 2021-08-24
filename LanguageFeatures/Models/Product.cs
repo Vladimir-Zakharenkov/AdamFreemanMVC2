c484d128cf3ca38158aa90cf1ff29d5912354aa7
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-08-24 10:38:58 +0300

    Assigning a Value to a Read-Only Property

--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -60,10 +60,71 @@ namespace LanguageFeatures.Models
 
         #region Using Auto-Implemented Property Initializers
 
+        //public string Name { get; set; }
+        //public string Category { get; set; } = "Watersports";
+        //public decimal? Price { get; set; }
+        //public Product Related { get; set; }
+        //public static Product[] GetProducts()
+        //{
+        //    Product kayak = new Product
+        //    {
+        //        Name = "Kayak",
+        //        Category = "Water Craft",
+        //        Price = 275M
+        //    };
+        //    Product lifejacket = new Product
+        //    {
+        //        Name = "Lifejacket",
+        //        Price = 48.95M
+        //    };
+        //    kayak.Related = lifejacket;
+        //    return new Product[] { kayak, lifejacket, null };
+        //}
+
+        #endregion
+
+        #region Creating a Read-Only Property
+
+        //public string Name { get; set; }
+        //public string Category { get; set; } = "Watersports";
+        //public decimal? Price { get; set; }
+        //public Product Related { get; set; }
+        //public bool InStock { get; } = true;
+
+        //public static Product[] GetProducts()
+        //{
+        //    Product kayak = new Product
+        //    {
+        //        Name = "Kayak",
+        //        Category = "Water Craft",
+        //        Price = 275M
+        //    };
+        //    Product lifejacket = new Product
+        //    {
+        //        Name = "Lifejacket",
+        //        Price = 48.95M
+        //    };
+
+        //    kayak.Related = lifejacket;
+
+        //    return new Product[] { kayak, lifejacket, null };
+        //}
+
+        #endregion
+
+        #region Assigning a Value to a Read-Only Property
+
+        public Product(bool stock = true)
+        {
+            InStock = stock;
+        }
+
         public string Name { get; set; }
         public string Category { get; set; } = "Watersports";
         public decimal? Price { get; set; }
         public Product Related { get; set; }
+        public bool InStock { get; }
+
         public static Product[] GetProducts()
         {
             Product kayak = new Product
@@ -72,16 +133,19 @@ namespace LanguageFeatures.Models
                 Category = "Water Craft",
                 Price = 275M
             };
-            Product lifejacket = new Product
+
+            Product lifejacket = new Product(false)
             {
                 Name = "Lifejacket",
                 Price = 48.95M
             };
-            kayak.Related = lifejacket;
-            return new Product[] { kayak, lifejacket, null };
-        }
 
-        #endregion
+            kayak.Related = lifejacket;
+
+            return new Product[] { kayak, lifejacket, null };
+
+            #endregion
+        }
 
     }
 }
