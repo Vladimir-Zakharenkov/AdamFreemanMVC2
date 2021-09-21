6196c482231c95ce4299d5248c274aa9dedff742
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-09-22 00:06:52 +0300

    Применение имитированных объектов в файле HomeControllerTests.

--- WorkingWithVisualStudio.Tests/HomeControllerTests.cs
+++ WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -3,31 +3,21 @@ using System.Collections.Generic;
 using WorkingWithVisualStudio.Controllers;
 using WorkingWithVisualStudio.Models;
 using Xunit;
+using System;
+using Moq;
 
 namespace WorkingWithVisualStudio.Tests
 {
     public class HomeControllerTests
     {
-        class ModelCompleteFakeRepository : IRepository
-        {
-            public IEnumerable<Product> Products { get; set; }
-
-            public void AddProduct(Product p)
-            {
-                // Ничего не делать - для теста не требуется
-            }
-        }
-
         [Theory]
         [ClassData(typeof(ProductTestData))]
         public void IndexActionIsComplete(Product[] products)
         {
             // Организация
-            var controller = new HomeController();
-            controller.Repository = new ModelCompleteFakeRepository
-            {
-                Products = products
-            };
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products).Returns(products);
+            var controller = new HomeController { Repository = mock.Object };
 
             // Действие
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
@@ -37,37 +27,19 @@ namespace WorkingWithVisualStudio.Tests
                 p1.Name == p2.Name && p1.Price == p2.Price));
         }
 
-        class PropertyOnceFakeRepository : IRepository
-        {
-            public int PropertyCounter { get; set; } = 0;
-
-            public IEnumerable<Product> Products
-            {
-                get
-                {
-                    PropertyCounter++;
-                    return new[] { new Product { Name = "P1", Price = 100 } };
-                }
-            }
-
-            public void AddProduct(Product p)
-            {
-                // Ничего не делать - для теста не требуется
-            }
-        }
-
         [Fact]
         public void RepositoryPropertyCalledOnce()
         {
             // Организация
-            var repo = new PropertyOnceFakeRepository();
-            var controller = new HomeController { Repository = repo };
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products).Returns(new[] { new Product { Name = "P1", Price = 100 } });
+            var controller = new HomeController { Repository = mock.Object };
 
             // Действие
             var result = controller.Index();
 
             // Утверждение
-            Assert.Equal(1, repo.PropertyCounter);
+            mock.VerifyGet(m => m.Products, Times.Once);
 
         }
     }
