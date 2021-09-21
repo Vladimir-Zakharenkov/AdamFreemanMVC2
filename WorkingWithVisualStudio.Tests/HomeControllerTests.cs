328642b664f64368a3c762136d3ead0073a42804
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-09-21 17:50:32 +0300

    Изменяем класс HomeControllerTests.cs

--- WorkingWithVisualStudio.Tests/HomeControllerTests.cs
+++ WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -8,17 +8,34 @@ namespace WorkingWithVisualStudio.Tests
 {
     public class HomeControllerTests
     {
+        class ModelCompleteFakeRepository : IRepository
+        {
+            public IEnumerable<Product> Products { get; } = new Product[]
+            {
+                new Product{ Name = "P1", Price = 275M } ,
+                new Product{ Name = "P2", Price = 48.95M },
+                new Product{ Name = "P3", Price = 19.50M },
+                new Product{ Name = "P4", Price = 34.95M }
+            };
+
+            public void AddProduct(Product p)
+            {
+                // Ничего не делать - для теста не требуется
+            }
+        }
+
         [Fact]
         public void IndexActionModelIsComplete()
         {
             // Организация
             var controller = new HomeController();
+            controller.Repository = new ModelCompleteFakeRepository();
 
             // Действие
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
 
             // Утверждение
-            Assert.Equal(SimpleRepository.SharedRepository.Products, model, Comparer.Get<Product>((p1, p2) =>
+            Assert.Equal(controller.Repository.Products, model, Comparer.Get<Product>((p1, p2) =>
                 p1.Name == p2.Name && p1.Price == p2.Price));
         }
     }
