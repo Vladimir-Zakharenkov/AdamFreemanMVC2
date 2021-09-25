d1c074ae1208da867748b6f842647b9f0aa34c31
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-09-26 01:05:48 +0300

    Создаем тест для проверки разбиения данных на страницы

---
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void Can_Paginate()
+        {
+            // Организация
+            Mock<IProductRepository> mock = new();
+
+            mock.Setup(x => x.Products).Returns((new Product[]
+            {
+                new Product { ProductId = 1, Name = "P1" },
+                new Product { ProductId = 2, Name = "P2"},
+                new Product { ProductId = 3, Name = "P3"},
+                new Product { ProductId = 4, Name = "P4"},
+                new Product { ProductId = 5, Name = "P5"}
+            }).AsQueryable<Product>);
+
+            ProductController controller = new (mock.Object);
+            controller.PageSize = 3;
+
+            // Действие
+            IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
+
+            // Утверждение
+            Product[] prodArray = result.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
+        }
+    }
+}
