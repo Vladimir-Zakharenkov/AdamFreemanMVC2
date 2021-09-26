1923964aaa3162ca00fd674b12606284ab7aed42
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-09-26 23:51:34 +0300

    Изменяем метод List() Вв контроллере ProductController.cs

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SportsStore.Models;
 using System.Linq;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+
 
 namespace SportsStore.Controllers
 {
@@ -15,9 +17,24 @@ namespace SportsStore.Controllers
             repository = repo;
         }
 
-        public ViewResult List(int productPage = 3) => View(repository.Products
-            .OrderBy(p => p.ProductId)
-            .Skip((productPage - 1) * PageSize)
-            .Take(PageSize));
+        //public ViewResult List(int productPage = 3) => View(repository.Products
+        //    .OrderBy(p => p.ProductId)
+        //    .Skip((productPage - 1) * PageSize)
+        //    .Take(PageSize));
+
+        public ViewResult List(int productPage = 1) =>
+            View(new ProductListViewModel
+            {
+                Products = repository.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new()
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
     }
 }
